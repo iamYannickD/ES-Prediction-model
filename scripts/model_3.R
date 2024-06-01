287dc76442d20981e7c41d9d402daa721448ca6d
iamyannickd <yannickdoungmo@gmail.com>
2024-06-01 16:38:20 +0100

    Pulled and merged data from 2016 to 2024 by using AFRO Data #5 , #5

--- scripts/model_3.R
+++ scripts/model_3.R
@@ -12,15 +12,15 @@ es_repo <- read_csv("../data/link/access.txt")
     data_links <- es_repo$lien[2:9]
     years <- c("2016_2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024")
     
-    # Create an empty list to store datasets
+# Create an empty list to store datasets
     es_data_list <- list()
     
-    # Load datasets using a for loop
+# Load datasets using a for loop
     for (i in seq_along(years)) {
-      es_data_list[[years[i]]] <- read_csv(data_links[i])
+      es_data_list[[paste0("es_", years[i])]] <- read_csv(data_links[i])
     }
     
-    # Assign datasets to variables by year
+# Assign datasets to variables by year
     list2env(es_data_list, .GlobalEnv)
   
 # load masterlist
@@ -29,12 +29,74 @@ es_repo <- read_csv("../data/link/access.txt")
     active_es_sites <-
         read_csv(masterlist) |>
         filter(STATUS == "ACTIVE") 
+    
+# List of common columns to select
+  common_columns <- c("Countryname", "Province", "District", "Sitename", "Sitecode", 
+                      "Dateofcollection", "Datesamplesenttolab", "Datesampleinlab", 
+                      "Samplecondition", "Finalcellcultureresult", "Datefinalcultureresult")
   
+# Handle the specific column name for Sitecode1 in es_2019 separately
+  columns_2019 <- c("Countryname", "Province", "District", "Sitename", "Sitecode1", 
+                    "Dateofcollection", "Datesamplesenttolab", "Datesampleinlab", 
+                    "Samplecondition", "Finalcellcultureresult", "Datefinalcultureresult")
   
+# Apply select to each dataset using columns names, and save result in a list
+# as the number of columns are not equal, the idea is to select some specific columns that we will use in the 
+  # analysis. Then, to avoid repeiting the same script, we create 2 variables filled with columns names and that
+  # we will use in the select script. Then, we use the for loop to iterate our selection on all the samples
+  for (year in names(es_data_list)) {
+    if (year == "es_2019") {
+      es_data_list[[year]] <- es_data_list[[year]] %>%
+        select(all_of(columns_2019))
+    } else {
+      es_data_list[[year]] <- es_data_list[[year]] %>%
+        select(all_of(common_columns))
+    }
+  }
+  
+# Assign datasets to variables by year
+  list2env(es_data_list, .GlobalEnv)
 
+# create a cumulative es file with all the results generated 
+  all_es_data <-
+    bind_rows(es_2016_2017, es_2018, es_2019, es_2020, es_2021, es_2022, es_2022, es_2024)
   
+# adding long lat to ES sites and other information from the masterlist
+  es_data <-
+    left_join(x = all_es_data, y = active_es_sites, by = c("Sitecode" = "SITE_CODE" )) |>
+    filter(
+      !is.na(COUNTRY), 
+      COUNTRY == "DEMOCRATIC REPUBLIC OF CONGO") |>
+    select(1:15, IST, SITE_NAME, Long_X, Lat_Y) |>
+    mutate(
+      numb_days = as.integer(dmy(Datesampleinlab) - dmy(Dateofcollection)),
+      year = year(dmy(Datesampleinlab)),
+      ep_week = as.numeric(epiweek(dmy(Datesampleinlab))),
+      ep_month = as.numeric(month(dmy(Datesampleinlab)))
+    )  
   
-  
+es_data |> 
+    filter(numb_days >= 0) |> 
+    group_by(COUNTRY, ep_month) |>
+    #summarise(median_days = median(numb_days), max_days = max(numb_days) , .groups = "drop") |>
+    mutate(
+      median_days = median(numb_days),
+      max_days = max(numb_days)) |>
+      mutate(
+        ep_week = as.factor(ep_week),
+        ep_month = as.factor(ep_month)) |>
+      ungroup() |> 
+    ggplot() +
+    #geom_boxplot(aes(x = numb_days, y = fct_reorder(PROVINCE, median_days))) 
+    #geom_density_ridges(aes(x = median_days, y = ep_week, fill = median_days ), scale = 0.9 ) + 
+    geom_density_ridges(aes(x = median_days, y = ep_month, fill = median_days ), scale = 0.9 ) +
+    facet_wrap(~year) +
+    labs(title = "Number of Days by Median Days and Months",
+         x = "Number of Days",
+         y = "Month",
+         fill = "Median Days") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))  
   
   
   
