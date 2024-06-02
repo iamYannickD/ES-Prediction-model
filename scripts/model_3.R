a4e8e75e337f2874855f5bc0c1d298ad9e955b96
iamyannickd <yannickdoungmo@gmail.com>
2024-06-02 01:40:00 +0100

    Update model_3.R

--- scripts/model_3.R
+++ scripts/model_3.R
@@ -36,7 +36,7 @@ es_repo <- read_csv("../data/link/access.txt")
                       "Samplecondition", "Finalcellcultureresult", "Datefinalcultureresult")
   
 # Handle the specific column name for Sitecode1 in es_2019 separately
-  columns_2019 <- c("Countryname", "Province", "District", "Sitename", "Sitecode1", 
+  columns_2019 <- c("Countryname", "Province", "District", "Sitename", "Sitecode...22", 
                     "Dateofcollection", "Datesamplesenttolab", "Datesampleinlab", 
                     "Samplecondition", "Finalcellcultureresult", "Datefinalcultureresult")
   
@@ -47,7 +47,8 @@ es_repo <- read_csv("../data/link/access.txt")
   for (year in names(es_data_list)) {
     if (year == "es_2019") {
       es_data_list[[year]] <- es_data_list[[year]] %>%
-        select(all_of(columns_2019))
+        select(all_of(columns_2019)) |>
+        rename(Sitecode = Sitecode...22)
     } else {
       es_data_list[[year]] <- es_data_list[[year]] %>%
         select(all_of(common_columns))
@@ -59,14 +60,20 @@ es_repo <- read_csv("../data/link/access.txt")
 
 # create a cumulative es file with all the results generated 
   all_es_data <-
-    bind_rows(es_2016_2017, es_2018, es_2019, es_2020, es_2021, es_2022, es_2022, es_2024)
+    bind_rows(es_2016_2017, es_2018, es_2019, es_2020, es_2021, es_2022, es_2022, es_2024) |>
+    arrange("datesampleinlab") |>
+    filter(Countryname == "CAMEROON" & year(Datesampleinlab) == 2023) 
+        
   
 # adding long lat to ES sites and other information from the masterlist
   es_data <-
     left_join(x = all_es_data, y = active_es_sites, by = c("Sitecode" = "SITE_CODE" )) |>
+    arrange("datesampleinlab") |>
+    filter(COUNTRY == "CAMEROON" & year(Datesampleinlab) == 2023) 
+  
     filter(
       !is.na(COUNTRY), 
-      COUNTRY == "DEMOCRATIC REPUBLIC OF CONGO") |>
+      COUNTRY == "CAMEROON") |>
     select(1:15, IST, SITE_NAME, Long_X, Lat_Y) |>
     mutate(
       numb_days = as.integer(dmy(Datesampleinlab) - dmy(Dateofcollection)),
